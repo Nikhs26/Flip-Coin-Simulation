61ed49e6c40db930366839317621d4128dc428c6
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-07 23:31:23 +0530

    show number of times head and tail has won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,28 @@
 #! /bin/bash
 
-flipCoinResult=$((RANDOM%2))
-IS_HEAD=1
+declare -A flipCoin
+read -p "How many times you want to flip coin " noOfTime
 
-if [ $flipCoinResult -eq $IS_HEAD ]
-then
-	echo "H"
-else
-	echo "T"
-fi
+IS_HEAD=1
+counter=0
+headCount=0
+tailCount=0
+
+while [ $counter -lt $noOfTime ]
+do
+	flipCoinResult=$((RANDOM%2))
+	if [ $flipCoinResult -eq $IS_HEAD ]
+        then
+                echo "Head"
+		flipCoin[$counter]="Head"
+		((headCount++))
+        else
+                echo "Tail"
+		flipCoin[$counter]="Tail"
+		((tailCount++))
+        fi
+((counter++))
+done
+
+echo "The number of times won the head: $headCount"
+echo "The number of times won the tail: $tailCount"
